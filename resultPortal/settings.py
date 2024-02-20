4fa2415d680779557553cbd9cc5fb3e0d9d0e52a
milemotigo <mtigo@gmail.com>
2024-02-20 22:39:43 +0100

    added staticfile dir

--- resultPortal/settings.py
+++ resultPortal/settings.py
@@ -138,4 +138,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
