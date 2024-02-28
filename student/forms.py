85db416846eb23a85ddc8df7b0689efc4d90a8ad
milemotigo <mtigo@gmail.com>
2024-02-28 17:29:39 +0100

    stident registration

--- student/forms.py
+++ student/forms.py
@@ -4,5 +4,5 @@ from student.models import Student
 class StudentRegistrationForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ['username', 'email', 'matric_number', 'address', 'bio', 'phone_number', 'birth_date', 'address', 'profile_picture']
+        fields = ['username', 'email', 'matric_number', 'address', 'phone_number', 'birth_date', 'address', 'profile_picture']
 
