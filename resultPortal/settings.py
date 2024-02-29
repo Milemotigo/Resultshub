ff1ed9ddd2824a821fd653556bcadfc8c275271a
milemotigo <mtigo@gmail.com>
2024-02-29 12:57:26 +0100

    registrahtion and login almost done

--- resultPortal/settings.py
+++ resultPortal/settings.py
@@ -140,8 +140,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-#AUTHENTICATION_BACKENDS = [
-#    'student.backends.MatnumberAuthBackend',
-#    'django.contrib.auth.backends.ModelBackend',
-#]  
+AUTHENTICATION_BACKENDS = [
+    'student.backends.MatnumberAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]  
 
